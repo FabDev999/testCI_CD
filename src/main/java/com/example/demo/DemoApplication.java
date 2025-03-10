8096c95de76d67dec0c7927fef10987a2f54f367
fabrizio.bellisai <f.bellisai@lunalabs.it>
2025-03-10 17:10:43 +0100

    WIP

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello test");
 	}
 
 }
